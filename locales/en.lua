bde7a580bf4e320c5940af50302609e2b9d210a6
lunatrik <lunatrik@outlook.com>
2021-06-25 23:16:35 +0200

    fixes % in en local file

--- locales/en.lua
+++ locales/en.lua
@@ -1,7 +1,7 @@
 Locales['en'] = {
-    ['client_main_event_takeOutDepot_notify_1'] = "Vehicle Off:Engine %s% Body: %s% Fuel: %s%",
-    ['client_main_event_takeOutDepot_notify_2'] = "Vehicle Off:Engine %s% Body: %s% Fuel: %s%",
-    ['client_main_event_takeOutDepot_notify_3'] = "Vehicle Off:Engine %s% Body: %s% Fuel: %s%",
+    ['client_main_event_takeOutDepot_notify_1'] = "Vehicle Off:Engine %s%% Body: %s%% Fuel: %s%%",
+    ['client_main_event_takeOutDepot_notify_2'] = "Vehicle Off:Engine %s%% Body: %s%% Fuel: %s%%",
+    ['client_main_event_takeOutDepot_notify_3'] = "Vehicle Off:Engine %s%% Body: %s%% Fuel: %s%%",
 
     ["MyVehicles"] = "My Vehicles",
     ["CloseMenu"] = "Close Menu",
@@ -10,9 +10,9 @@ Locales['en'] = {
 
     ["client_main_function_HouseGarage_notify_1"] ="You have no vehicles in your garage",
 
-    ["client_main_motor"] =" Motor: %s%",
-    ["client_main_body"] =" Body: %s%",
-    ["client_main_fuel"] =" Fuel: %s%",
+    ["client_main_motor"] =" Motor: %s%%",
+    ["client_main_body"] =" Body: %s%%",
+    ["client_main_fuel"] =" Fuel: %s%%",
 
     
     ["client_main_function_DepotList_menutitle"] = "Impounded Vehicles :",
@@ -21,7 +21,7 @@ Locales['en'] = {
     ["client_main_function_VehicleList_notify_1"] = "You have no vehicles in this garage",
     ["client_main_function_GangVehicleList_notify_1"] = "You have no vehicles in this garage",
 
-    ["client_main_function_TakeOutVehicle_notify_1"] = "Vehicle Off:Engine %s% Body: %s% Fuel: %s%",
+    ["client_main_function_TakeOutVehicle_notify_1"] = "Vehicle Off:Engine %s%% Body: %s%% Fuel: %s%%",
     ["client_main_function_TakeOutVehicle_notify_2"] = "Is your vehicle in the Depot",
     ["client_main_function_TakeOutVehicle_notify3"] = "This vehicle was impounded by the Police",
 
