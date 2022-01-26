5b83043281f6cbf497ed48df038ef962997cefdc
aj <92224515+aj-devs@users.noreply.github.com>
2022-01-26 18:59:28 +0100

    Grammar
    Changed some Grammar to make it a little bit better

--- locales/de.lua
+++ locales/de.lua
@@ -1,6 +1,6 @@
 local Translations = {
     error = {
-        not_enough = "Nicht genu Geld",
+        not_enough = "Nicht genug Geld",
         not_impound = "Dein Auto ist nicht abgeschleppt",
         not_owned = "Auto nicht in besitz",
         no_vehicles = "Du hast keine Autos in dieser Garage!",
@@ -13,24 +13,24 @@ local Translations = {
         vehicle_parked = "Auto geparkt",
     },
     info = {
-        public_garage = "Öffentliche  Garage",
+        public_garage = "Öffentliche Garage",
         gang_garage = "Gang Garage",
         job_garage = "Job Garage",
         house_garage = "Haus Garage",
         impound = "Abschlepphof",
         my_vehicles = "Meine Autos",
-        view_stored = "Sieh deine geparkten Autos!",
+        view_stored = "Siehe deine geparkten Autos!",
         leave_garage = "⬅ Verlasse Garage",
         leave_depot = "⬅ Verlasse Depot",
         garage = "Garage: %{value}",
         depot = "Depot: %{value}",
         out = "Draußen",
         garaged = "In der Garage",
-        impounded = "Abgeschleppt durch Polizei",
+        impounded = "Abgeschleppt durch die Polizei",
         garage_line = "Ort: %{value}<br>Benzin: %{value2} | Motor: %{value3} | Karosserie: %{value4}",
         depot_line = "Kennzeichen: %{value}<br>Benzin: %{value2} | Motor: %{value3} | Karosserie: %{value4}",
         garage_e = "~g~E~w~ - Garage",
-        park_e = "~g~E~w~ - parke Auto",
+        park_e = "~g~E~w~ - parke das Auto",
     }
 }
 
