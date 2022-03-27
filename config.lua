4baac15229a34a8d46a7f7af258da792cb1f41db
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2022-03-27 06:30:56 +0200

    typo fix

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 AutoRespawn = false --True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
 SharedGarages = false   --True == Gang and job garages are shared, false == Gang and Job garages are personal
-WrapIntoVehicle = false -- True == If you spawn a vehicle, you will be teleported into it
+WarpIntoVehicle = false -- True == If you spawn a vehicle, you will be teleported into it
 
 Garages = {
     ["motelgarage"] = {
