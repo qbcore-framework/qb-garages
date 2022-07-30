b14904e37387f3568fef241ebeefe9ac458fd8d6
adweex <adweex@gmail.com>
2022-07-30 19:18:04 +0200

    Fixed QB not saving VehicleProperties on garage save. Fixed Car Damage, no more simulation, it will load from stored data (tires, windows, doors) This function was missing in the past 1 year...

--- client/main.lua
+++ client/main.lua
@@ -198,44 +198,28 @@ end
 local function doCarDamage(currentVehicle, veh)
     local engine = veh.engine + 0.0
     local body = veh.body + 0.0
+    local data = json.decode(veh.mods)
+    local doorStatus = data.doorStatus
+    local tireBurstState = data.tireBurstState
+    local windowStatus = data.windowStatus
 
-    Wait(100)
-    if VisuallyDamageCars then
-        if body < 900.0 then
-            SmashVehicleWindow(currentVehicle, 0)
-            SmashVehicleWindow(currentVehicle, 1)
-            SmashVehicleWindow(currentVehicle, 2)
-            SmashVehicleWindow(currentVehicle, 3)
-            SmashVehicleWindow(currentVehicle, 4)
-            SmashVehicleWindow(currentVehicle, 5)
-            SmashVehicleWindow(currentVehicle, 6)
-            SmashVehicleWindow(currentVehicle, 7)
+    for k, v in pairs(data.doorStatus) do
+        if v then
+            SetVehicleDoorBroken(currentVehicle, tonumber(k), true)
         end
-        if body < 800.0 then
-            SetVehicleDoorBroken(currentVehicle, 0, true)
-            SetVehicleDoorBroken(currentVehicle, 1, true)
-            SetVehicleDoorBroken(currentVehicle, 2, true)
-            SetVehicleDoorBroken(currentVehicle, 3, true)
-            SetVehicleDoorBroken(currentVehicle, 4, true)
-            SetVehicleDoorBroken(currentVehicle, 5, true)
-            SetVehicleDoorBroken(currentVehicle, 6, true)
+    end
+    for k, v in pairs(data.tireBurstState) do
+        if v then
+            SetVehicleTyreBurst(currentVehicle, tonumber(k), true)
         end
-        if engine < 700.0 then
-            SetVehicleTyreBurst(currentVehicle, 1, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 2, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 3, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 4, false, 990.0)
-        end
-        if engine < 500.0 then
-            SetVehicleTyreBurst(currentVehicle, 0, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 5, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 6, false, 990.0)
-            SetVehicleTyreBurst(currentVehicle, 7, false, 990.0)
+    end
+    for k, v in pairs(data.windowStatus) do
+        if not v then
+            SmashVehicleWindow(currentVehicle, tonumber(k))
         end
     end
     SetVehicleEngineHealth(currentVehicle, engine)
     SetVehicleBodyHealth(currentVehicle, body)
-
 end
 
 local function CheckPlayers(vehicle, garage)
@@ -386,6 +370,7 @@ local function enterVehicle(veh, indexgarage, type, garage)
                 local bodyDamage = math.ceil(GetVehicleBodyHealth(veh))
                 local engineDamage = math.ceil(GetVehicleEngineHealth(veh))
                 local totalFuel = exports['LegacyFuel']:GetFuel(veh)
+                TriggerServerEvent("qb-vehicletuning:server:SaveVehicleProps", QBCore.Functions.GetVehicleProperties(veh))
                 TriggerServerEvent('qb-garage:server:updateVehicle', 1, totalFuel, engineDamage, bodyDamage, plate, indexgarage, type, PlayerGang.name)
                 CheckPlayers(veh, garage)
                 if type == "house" then
