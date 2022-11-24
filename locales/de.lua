a12d5ad1ef40aa2e18328dfd8cd143f9685afb34
bluecraank <bluecraank@outlook.de>
2022-11-24 19:45:27 +0100

    Update de.lua
    ~g~, ~w~ not working in this message type
    no_vehicles keys changed to german

--- locales/de.lua
+++ locales/de.lua
@@ -1,6 +1,6 @@
 local Translations = {
     error = {
-        no_vehicles = "There is no vehicles in this location!",
+        no_vehicles = "In dieser Garage hast du kein Fahrzeug!",
         not_impound = "Dein Auto wurde nicht abgeschleppt",
         not_owned = "Das Fahrzeug gehört dir nicht",
         not_correct_type = "Du kannst dieses Fahrzeug hier nicht parken. (Falscher Parkplatz)",
@@ -46,10 +46,10 @@ local Translations = {
         impound = "Abgeschleppt von der Polizei",
     },
     info = {
-        car_e = "~g~E~w~ - Garage",
-        sea_e = "~g~E~w~ - Bootshause",
-        air_e = "~g~E~w~ - Hangar",
-        park_e = "~g~E~w~ - Fahrzeug einparken",
+        car_e = "[E] - Garage",
+        sea_e = "[E] - Bootshause",
+        air_e = "[E] - Hangar",
+        park_e = "[E] - Fahrzeug einparken",
         house_garage = "Haus Garage",
     }
 }
