d4e09c567ea425565ac8a5c1fc9bafab5d93e4f0
Worapol Buraphan <worapol.pan.buraphan@gmail.com>
2023-01-26 01:45:17 +0700

    feat: add shared public garage config

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = Config or {}
 Config.AutoRespawn = false --True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
 Config.SharedGarages = false   --True == Gang and job garages are shared, false == Gang and Job garages are personal
 Config.VisuallyDamageCars = true --True == Visually damage cars that go out of the garage depending of body damage, false == Do not visually damage cars (damage is still applied to car values)
+Config.SharedPublicGarages = true
 
 Config.Garages = {
     ["motelgarage"] = {
