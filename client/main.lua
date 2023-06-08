9b195202ce214c152bcf3afd512f9bc72dfb631a
Luna <Sharkiiie@users.noreply.github.com>
2023-06-07 22:59:08 -0500

    fix(markers): Crash on nil `putVehicle`

--- client/main.lua
+++ client/main.lua
@@ -486,7 +486,9 @@ CreateThread(function()
         sleep = 2000
         if currentGarage ~= nil then
             if Markers then
-                DrawMarker(2, currentGarage.putVehicle.x, currentGarage.putVehicle.y, currentGarage.putVehicle.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, 0.15, 255, 255, 255, 255, false, false, false, true, false, false, false)
+                if currentGarage.putVehicle then
+                    DrawMarker(2, currentGarage.putVehicle.x, currentGarage.putVehicle.y, currentGarage.putVehicle.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, 0.15, 255, 255, 255, 255, false, false, false, true, false, false, false)
+                end
                 DrawMarker(2, currentGarage.takeVehicle.x, currentGarage.takeVehicle.y, currentGarage.takeVehicle.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, 0.15, 200, 0, 0, 222, false, false, false, true, false, false, false)
                 sleep = 0
             elseif HouseMarkers then
